1acac364ebb9a6e6c06bcc24199cf93db32d9feb
joao-vls <joa.v55j@gmail.com>
2023-09-17 13:20:01 -0300

    detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,29 +5,104 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+function createDetailFull(pokemon) {
+    let detailFull = document.createElement("section");
+    detailFull.classList.add("detailPokemon");
+    detailFull.innerHTML =
+    `<div class="detailPokemonContainer">
+        <div class="detailPokemonImage ${pokemon.type}">
+            <div class="divButton">
+                <button class="closeDetail">X</button>
+                <button><img src="assets/img/cora.png"
+                alt="bookmark"></button>
             </div>
-        </li>
-    `
+            <div class="detailPokemonIdent">
+                <span class="name">${pokemon.name}</span>
+                <span class="number">#${pokemon.number}</span>
+                <div class="detailPokemonTypes">
+                    ${pokemon.types.map((type) => `<span class="type ${type}">${type}</span>`).join('')}
+                </div>
+            </div>
+            <img draggable="false"
+                src="${pokemon.photo}"
+                alt="${pokemon.nome}">
+        </div>
+        <div class="detailPokemonInfos">
+            <div class="divButton">
+                <button id="Desc">Description
+                <span class="lineAfter  ${pokemon.type}" ></span>
+                </button>
+                <button id="About">About
+                <span class="lineAfter  ${pokemon.type}" ></span>
+                </button>
+                <button id="BaseSt">Base Stats
+                <span class="lineAfter  ${pokemon.type}" ></span>
+                </button>
+            </div>
+        <ol>
+        <h4>Description</h4>
+        <br>
+        ${pokemon.description.description}
+        </ol>
+        </div>
+    </div>`
+    document.body.appendChild(detailFull);
+
+    const closeDetail = document.querySelector(".closeDetail");
+    const aboutButton = document.getElementById("About");
+    const baseButton = document.getElementById("BaseSt");
+    const descButton = document.getElementById("Desc");
+    descButton.addEventListener("click",()=>{document.querySelector(".detailPokemonInfos ol").innerHTML =`
+    <h4>Description</h4>
+    <br>
+    ${pokemon.description.description}`;});
+    baseButton.addEventListener("click",()=>{
+            document.querySelector(".detailPokemonInfos ol").innerHTML =`
+            ${pokemon.stats.map((stat) => `<li ><span class="stat">${stat.name}</span> <span class="value">${stat.value}</span>   
+            <div class="grafic"><div class="line" style="width:${stat.value}px; ${(stat.value<50) ? "background-color:red": ""}"> </div> </div></li>`).join('')}`;
+    });
+    descButton.focus();
+    aboutButton.addEventListener("click",()=>{
+            document.querySelector(".detailPokemonInfos ol").innerHTML =`
+            <h4>Abilities</h4>
+            ${pokemon.abilities.map((ability) => `<li >${ability.name}</li>`).join('')}
+            <h4>Caracter</h4>
+            <li >Height: ${pokemon.caracter[0]}cm</li>
+            <li >Weight: ${pokemon.caracter[1]}kg</li>
+            <li >Base Happiness : ${pokemon.description.base_happiness}</li>
+            <li >Capture rate : ${pokemon.description.capture_rate}</li>
+            `;
+    });
+    closeDetail.addEventListener("click",()=>{
+        document.body.style.overflow = "scroll";
+        document.body.removeChild(detailFull)});
+}
+
+function createPokemonToLi(pokemon) {
+    let liPai = document.createElement("li");
+    liPai.classList.add("pokemon", pokemon.type);
+    liPai.innerHTML = `   
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+        </ol>
+
+        <img src="${pokemon.photo}"
+             alt="${pokemon.name}">
+    </div>`
+    liPai.addEventListener("click",()=>{
+        document.body.style.overflow = "hidden";
+        createDetailFull(pokemon)});
+    pokemonList.appendChild(liPai);
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        pokemons.map(createPokemonToLi)
+    });
 }
 
 loadPokemonItens(offset, limit)
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -8,18 +8,43 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-
+    pokemon.stats = pokeDetail.stats.map((stat) => {
+        return {
+            name: stat.stat.name,
+            value: stat.base_stat
+        }
+    })
+    pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => {
+        return {
+            name: abilitySlot.ability.name
+        }
+    })
+    pokemon.description = pokeDetail.description
+    pokemon.caracter= [pokeDetail.height * 10,pokeDetail.weight / 10]
     pokemon.types = types
     pokemon.type = type
-
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
     return pokemon
 }
-
+pokeApi.getPokemonsDetailSpecie = (pokemon) => {
+    return fetch(pokemon.species.url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody)
+        .then((specie) => {
+            const flavorTextEntries = specie.flavor_text_entries
+            const newPokemon = pokemon
+            newPokemon.description = {
+                base_happiness: specie.base_happiness,
+                capture_rate: specie.capture_rate,
+                description: flavorTextEntries.find((flavorText) => flavorText.language.name === 'en').flavor_text,
+            }
+            return newPokemon;
+        })
+}
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(pokeApi.getPokemonsDetailSpecie)
         .then(convertPokeApiDetailToPokemon)
 }
 
